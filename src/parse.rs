c6e2e362c40c6dcb045a8f9e5c8f4510e4a26f7a
AsterixxxGallier <asterixxxgallier@aol.com>
2026-04-01 18:08:32 +0200

    Add some sugar to the syntax (literals can often be omitted)

--- src/parse.rs
+++ src/parse.rs
@@ -7,19 +7,19 @@ use pest_derive::Parser;
 #[grammar = "syntax.pest"]
 struct MyParser;
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub(crate) struct Signature<'s>(pub(crate) &'s str);
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub(crate) struct Function<'s>(pub(crate) &'s str);
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub(crate) struct SignatureLiteral<'s>(pub(crate) &'s str);
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub(crate) struct FunctionLiteral<'s>(pub(crate) &'s str);
 
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub(crate) enum ConjureDependency<'s> {
     Signature(Signature<'s>),
     Function(Function<'s>),
@@ -175,6 +175,22 @@ fn parse_function_literal(pair: Pair<Rule>) -> FunctionLiteral {
     FunctionLiteral(symbol.as_str())
 }
 
+fn parse_maybe_signature_literal<'s>(pair: Pair<'s, Rule>, default: &'s str) -> SignatureLiteral<'s> {
+    if let Some(literal) = pair.into_inner().next() {
+        parse_signature_literal(literal)
+    } else {
+        SignatureLiteral(default)
+    }
+}
+
+fn parse_maybe_function_literal<'s>(pair: Pair<'s, Rule>, default: &'s str) -> FunctionLiteral<'s> {
+    if let Some(literal) = pair.into_inner().next() {
+        parse_function_literal(literal)
+    } else {
+        FunctionLiteral(default)
+    }
+}
+
 fn parse_conjure_dependency(pair: Pair<Rule>) -> ConjureDependency {
     let rule = pair.as_rule();
     let (symbol,) = pair.into_inner().collect_tuple().unwrap();
@@ -193,10 +209,10 @@ fn parse_conjure_dependencies(pair: Pair<Rule>) -> ConjureDependencies {
     ConjureDependencies(dependencies)
 }
 
-fn parse_take_signature(pair: Pair<Rule>) -> TakeSignature {
+fn parse_take_signature<'s>(pair: Pair<'s, Rule>, default_literal: &'s str) -> TakeSignature<'s> {
     let (literal,) = pair.into_inner().collect_tuple().unwrap();
     TakeSignature {
-        literal: parse_signature_literal(literal),
+        literal: parse_maybe_signature_literal(literal, default_literal),
     }
 }
 
@@ -214,37 +230,39 @@ fn parse_define_signature(pair: Pair<Rule>) -> DefineSignature {
     }
 }
 
-fn parse_take_signature_from(pair: Pair<Rule>) -> TakeSignatureFrom {
+fn parse_take_signature_from<'s>(pair: Pair<'s, Rule>, default_literal: &'s str) -> TakeSignatureFrom<'s> {
     let (literal, source) = pair.into_inner().collect_tuple().unwrap();
     TakeSignatureFrom {
-        literal: parse_signature_literal(literal),
+        literal: parse_maybe_signature_literal(literal, default_literal),
         source: parse_function(source),
     }
 }
 
 fn parse_give_signature_to_signature(pair: Pair<Rule>) -> GiveSignatureToSignature {
     let (local, literal, source) = pair.into_inner().collect_tuple().unwrap();
+    let signature = parse_signature(local);
     GiveSignatureToSignature {
-        signature: parse_signature(local),
-        literal: parse_signature_literal(literal),
+        signature,
+        literal: parse_maybe_signature_literal(literal, signature.0),
         source: parse_signature(source),
     }
 }
 
 fn parse_give_function_to_signature(pair: Pair<Rule>) -> GiveFunctionToSignature {
     let (local, literal, source) = pair.into_inner().collect_tuple().unwrap();
+    let function = parse_function(local);
     GiveFunctionToSignature {
-        function: parse_function(local),
-        literal: parse_function_literal(literal),
+        function,
+        literal: parse_maybe_function_literal(literal, function.0),
         source: parse_signature(source),
     }
 }
 
-fn parse_take_function(pair: Pair<Rule>) -> TakeFunction {
+fn parse_take_function<'s>(pair: Pair<'s, Rule>, default_literal: &'s str) -> TakeFunction<'s> {
     let (signature, literal) = pair.into_inner().collect_tuple().unwrap();
     TakeFunction {
         signature: parse_signature(signature),
-        literal: parse_function_literal(literal),
+        literal: parse_maybe_function_literal(literal, default_literal),
     }
 }
 
@@ -263,39 +281,41 @@ fn parse_define_function(pair: Pair<Rule>) -> DefineFunction {
     }
 }
 
-fn parse_take_function_from(pair: Pair<Rule>) -> TakeFunctionFrom {
+fn parse_take_function_from<'s>(pair: Pair<'s, Rule>, default_literal: &'s str) -> TakeFunctionFrom<'s> {
     let (literal, source) = pair.into_inner().collect_tuple().unwrap();
     TakeFunctionFrom {
-        literal: parse_function_literal(literal),
+        literal: parse_maybe_function_literal(literal, default_literal),
         source: parse_function(source),
     }
 }
 
 fn parse_give_signature_to_function(pair: Pair<Rule>) -> GiveSignatureToFunction {
     let (local, literal, source) = pair.into_inner().collect_tuple().unwrap();
+    let signature = parse_signature(local);
     GiveSignatureToFunction {
-        signature: parse_signature(local),
-        literal: parse_signature_literal(literal),
+        signature,
+        literal: parse_maybe_signature_literal(literal, signature.0),
         source: parse_function(source),
     }
 }
 
 fn parse_give_function_to_function(pair: Pair<Rule>) -> GiveFunctionToFunction {
     let (local, literal, source) = pair.into_inner().collect_tuple().unwrap();
+    let function = parse_function(local);
     GiveFunctionToFunction {
-        function: parse_function(local),
-        literal: parse_function_literal(literal),
+        function,
+        literal: parse_maybe_function_literal(literal, function.0),
         source: parse_function(source),
     }
 }
 
-fn parse_signature_assignment_rhs(pair: Pair<Rule>) -> SignatureAssignmentRhs {
+fn parse_signature_assignment_rhs<'s>(pair: Pair<'s, Rule>, lhs: &'s str) -> SignatureAssignmentRhs<'s> {
     match pair.as_rule() {
-        Rule::take_signature => SignatureAssignmentRhs::Take(parse_take_signature(pair)),
+        Rule::take_signature => SignatureAssignmentRhs::Take(parse_take_signature(pair, lhs)),
         Rule::conjure_signature => SignatureAssignmentRhs::Conjure(parse_conjure_signature(pair)),
         Rule::define_signature => SignatureAssignmentRhs::Define(parse_define_signature(pair)),
         Rule::take_signature_from => {
-            SignatureAssignmentRhs::TakeFrom(parse_take_signature_from(pair))
+            SignatureAssignmentRhs::TakeFrom(parse_take_signature_from(pair, lhs))
         }
         Rule::give_signature_to_signature => {
             SignatureAssignmentRhs::GiveSignatureToSignature(parse_give_signature_to_signature(pair))
@@ -307,12 +327,12 @@ fn parse_signature_assignment_rhs(pair: Pair<Rule>) -> SignatureAssignmentRhs {
     }
 }
 
-fn parse_function_assignment_rhs(pair: Pair<Rule>) -> FunctionAssignmentRhs {
+fn parse_function_assignment_rhs<'s>(pair: Pair<'s, Rule>, lhs: &'s str) -> FunctionAssignmentRhs<'s> {
     match pair.as_rule() {
-        Rule::take_function => FunctionAssignmentRhs::Take(parse_take_function(pair)),
+        Rule::take_function => FunctionAssignmentRhs::Take(parse_take_function(pair, lhs)),
         Rule::conjure_function => FunctionAssignmentRhs::Conjure(parse_conjure_function(pair)),
         Rule::define_function => FunctionAssignmentRhs::Define(parse_define_function(pair)),
-        Rule::take_function_from => FunctionAssignmentRhs::TakeFrom(parse_take_function_from(pair)),
+        Rule::take_function_from => FunctionAssignmentRhs::TakeFrom(parse_take_function_from(pair, lhs)),
         Rule::give_signature_to_function => {
             FunctionAssignmentRhs::GiveSignatureToFunction(parse_give_signature_to_function(pair))
         }
@@ -325,33 +345,37 @@ fn parse_function_assignment_rhs(pair: Pair<Rule>) -> FunctionAssignmentRhs {
 
 fn parse_signature_assignment(pair: Pair<Rule>) -> SignatureAssignment {
     let (lhs_pair, rhs_pair) = pair.into_inner().collect_tuple().unwrap();
+    let lhs = parse_signature(lhs_pair);
     SignatureAssignment {
-        lhs: parse_signature(lhs_pair),
-        rhs: parse_signature_assignment_rhs(rhs_pair),
+        lhs,
+        rhs: parse_signature_assignment_rhs(rhs_pair, lhs.0),
     }
 }
 
 fn parse_function_assignment(pair: Pair<Rule>) -> FunctionAssignment {
     let (lhs_pair, rhs_pair) = pair.into_inner().collect_tuple().unwrap();
+    let lhs = parse_function(lhs_pair);
     FunctionAssignment {
-        lhs: parse_function(lhs_pair),
-        rhs: parse_function_assignment_rhs(rhs_pair),
+        lhs,
+        rhs: parse_function_assignment_rhs(rhs_pair, lhs.0),
     }
 }
 
 fn parse_give_signature(pair: Pair<Rule>) -> GiveSignature {
     let (signature, literal) = pair.into_inner().collect_tuple().unwrap();
+    let signature = parse_signature(signature);
     GiveSignature {
-        signature: parse_signature(signature),
-        literal: parse_signature_literal(literal),
+        signature,
+        literal: parse_maybe_signature_literal(literal, signature.0),
     }
 }
 
 fn parse_give_function(pair: Pair<Rule>) -> GiveFunction {
     let (function, literal) = pair.into_inner().collect_tuple().unwrap();
+    let function = parse_function(function);
     GiveFunction {
-        function: parse_function(function),
-        literal: parse_function_literal(literal),
+        function,
+        literal: parse_maybe_function_literal(literal, function.0),
     }
 }
 
